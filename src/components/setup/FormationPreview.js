db4fb088d804a36c34325828d640e66e50b61e1a
beggan78 <beggan78@gmail.com>
2025-08-10 00:20:29 +0200

    Use original formation preview image

--- src/components/setup/FormationPreview.js
+++ src/components/setup/FormationPreview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Shield, Sword, Hand, ArrowDownUp } from 'lucide-react';
 import { FORMATIONS } from '../../constants/teamConfiguration';
-import fieldImage from '../../assets/images/full-field-perspective-small.png';
+import fieldImage from '../../assets/images/full-field-perspective.png';
 
 /**
  * FormationPreview - Visual preview component for tactical formations
